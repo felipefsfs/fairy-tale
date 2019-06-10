067a308dd4d181b447538809407b42b1e241b941
felipefsfs <Felipe>
2019-06-10 16:47:00 -0300

    BASE NAVBAR

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
 //import 'materialize-css/dist/css/materialize.min.css';
+import { BrowserRouter } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/nav.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Fairy Tale</h1>
-      <i className="material-icons">assessment</i>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <div className="App">
+            <Nav />
+            <i className="material-icons">assessment</i>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,1 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+    return (
+        <nav className="nav-wrapper grey">
+            <div className="container">
+                <Link to="/" className="brand-logo">Fairy Tale</Link>
+            </div>
+        </nav>
+    );
+}
+  
+export default Nav;
