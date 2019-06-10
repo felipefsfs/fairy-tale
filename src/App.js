20046f046d3b212bc0962759a30bfecc53d9afe6
felipefsfs <Felipe>
2019-06-10 17:35:16 -0300

    factored out the splash page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React from 'react';
 //import 'materialize-css/dist/css/materialize.min.css';
-import { BrowserRouter } from "react-router-dom";
-
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
+import Splash from "./components/splash.js";
+import Empty from "./components/empty.js";
 import Nav from "./components/nav.js";
 
+import fff, * as g from "./components/e.js";
+
+fff();
+g.default();
 function App() {
   return (
     <BrowserRouter>
         <div className="App">
             <Nav />
-            <i className="material-icons">assessment</i>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Switch>
+                <Route to="/" component={Splash} />
+                <Route to="/e" component={Empty} />
+            </Switch>
         </div>
     </BrowserRouter>
   );
