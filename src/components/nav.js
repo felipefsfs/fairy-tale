b591809d9b5589ea87256a6727f4a341e6857330
felipefsfs <Felipe>
2019-06-12 09:44:26 -0300

    clean up splash screen

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,7 +6,7 @@ export default function Nav() {
     return (
         <nav className="nav-wrapper grey">
             <div className="container">
-                <Link to="/" className="brand-logo">Fairy Tale</Link>
+                <Link to="/" className="left brand-logo">Fairy Tale</Link>
                 <NavLinks />
             </div>     
         </nav>
--- src/components/splash.js
+++ src/components/splash.js
@@ -6,16 +6,8 @@ export default function Splash() {
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-                Edit <code>src/App.js</code> and save to reload.
+                Accounting TOOL!
             </p>
-            <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
         </header>
     );
 }
