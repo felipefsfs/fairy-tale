e4392b9846d7c7089e04adbdedbefeda3a9132b3
felipefsfs <Felipe>
2019-06-22 10:44:17 -0300

    sign in working

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { CurrentUserContext } from "../stores/CurrentUser";
 
 export default function SignIn() {
     const [email, set_email] = useState("");
     const [password, set_password] = useState("");
+
+    const current = useContext(CurrentUserContext);
 
     return (
         <div className="container">
@@ -24,11 +27,12 @@ export default function SignIn() {
         </div>
     );
 
-    function submit(e) {
-        e.preventDefault();
-        console.log(e);
+    function submit(event) {
+        event.preventDefault();
+        console.log(event);
         console.log(email);
         console.log(password);
+        current.create(email,password);
     }
 
     function handleInput(setter) {
