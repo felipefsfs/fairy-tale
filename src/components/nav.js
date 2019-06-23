6cc4a4fb31c5d906726c276497ed2d6e9de1c9f7
felipefsfs <Felipe>
2019-06-23 04:05:56 -0300

    nav using context

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import NavLinks from "./navLinks";
+import { CurrentUserContext } from "../stores/CurrentUser";
 
 export default function Nav() {
+    const current = useContext(CurrentUserContext);
     return (
         <nav className="nav-wrapper grey">
             <div className="container">
-                <Link to="/" className="left brand-logo">Fairy Tale</Link>
+                <Link to="/" className="left brand-logo">{"Fairy Tale " + 
+                    ((current.user||{}).displayName || "")}</Link>
                 <NavLinks />
             </div>     
         </nav>
