9ed19e6dfb33db57a753095a826694f3a721c442
felipefsfs <Felipe>
2019-06-23 12:55:57 -0300

    loading indicator component

---
+++ src/components/Loading.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Loading({size}) {
+  return (
+    <div className={"preloader-wrapper active " + 
+        ((size === "big" && size)||
+        (size === "small" && size)||
+        "")}>
+      <div className="spinner-layer">
+        <div className="circle-clipper left">
+          <div className="circle"></div>
+        </div><div className="gap-patch">
+          <div className="circle"></div>
+        </div><div className="circle-clipper right">
+          <div className="circle"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
