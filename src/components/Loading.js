1699a1fd85edaf5a4ddcef1c21c25864becb68f7
felipefsfs <Felipe>
2019-06-23 15:28:51 -0300

    upd Loading classes

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Loading({size}) {
   return (
-    <div className={"preloader-wrapper active " + 
-        ((size === "big" && size)||
-        (size === "small" && size)||
-        "")}>
+    <div className={`preloader-wrapper active
+      ${size === "big" || size}
+      ${size === "small" || size}
+    `}>
       <div className="spinner-layer">
         <div className="circle-clipper left">
           <div className="circle"></div>
